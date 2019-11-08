53a0917cd24c8bb091501c837ef45af87c58d309
Saad Khan <sk6786@g.rit.edu>
2019-11-08 15:01:50 -0500

    prevent same users

--- controller/auth.py
+++ controller/auth.py
@@ -6,8 +6,14 @@ class Auth():
     def __init__(self):
         self.__client = pymongo.MongoClient("mongodb+srv://"+urllib.parse.quote_plus("USER2")+":"+urllib.parse.quote_plus("1q2w3e4r")+"@cluster0-tk7v1.mongodb.net/test?retryWrites=true&w=majority")
 
-    def user_exists(self, username, password):
+    def login(self, username, password):
         db = self.__client.get_database("SAM2020")
         col = db.get_collection("Accounts")
         res = col.find_one({"username": username, "password": password})
+        return 0 if res is None else 1
+
+    def user_exists(self, username):
+        db = self.__client.get_database("SAM2020")
+        col = db.get_collection("Accounts")
+        res = col.find_one({"username": username})
         return 0 if res is None else 1
