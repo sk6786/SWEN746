005f3b53875de0e1ecb3f96c89449d35f027e392
Saad Khan <sk6786@g.rit.edu>
2019-11-11 15:25:34 -0500

    Added paper upload

--- model/account_pkg/author_account.py
+++ model/account_pkg/author_account.py
@@ -1,12 +1,16 @@
 from model.account_pkg.account import Account
-
+import pymongo
+from pymongo import MongoClient
+import urllib.parse
 
 class AuthorAccount(Account):
 
-    def __init__(self, account_id: int, username: str, password: str):
-        self.__account_id = account_id
-        self.__username = username
-        self.__password = password
+    def __init__(self):
+        self.__account_id = ""
+        self.__username = ""
+        self.__password = ""
+        self.__client = pymongo.MongoClient("mongodb+srv://"+urllib.parse.quote_plus("USER2")+":"+urllib.parse.quote_plus("1q2w3e4r")+"@cluster0-tk7v1.mongodb.net/test?retryWrites=true&w=majority")
+
 
     @property
     def __account_id(self):
@@ -18,8 +22,35 @@ class AuthorAccount(Account):
 
     # TODO: add other getter and setter
 
-    def login(self, username: str, password: str):
-        pass
+    def login(self, username, password):
+        db = self.__client.get_database("SAM2020")
+        col = db.get_collection("Accounts")
+        res = col.find_one({"username": username, "password": password})
+        if res is None:
+            return 0
+        else:
+            self.__username = username
+            self.__account_id = res['_id']
+            self.__password = password
+            return 1
+
+    def user_exists(self, username):
+        db = self.__client.get_database("SAM2020")
+        col = db.get_collection("Accounts")
+        res = col.find_one({"username": username})
+        return 0 if res is None else 1
+
+    def account_type(self, username):
+        db = self.__client.get_database("SAM2020")
+        col = db.get_collection("Accounts")
+        res = col.find_one({"username": username})
+        return res['role']
+
+    def create_account(self, username, password):
+        db = self.__client.get_database("SAM2020")
+        col = db.get_collection("Accounts")
+        mydict = {"username": username, "password": password, "role": "author"}
+        col.insert_one(mydict)
 
     def change_password(self, oldpass: str, newpass: str):
         pass
