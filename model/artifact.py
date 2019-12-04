3f26f13aa22dee60c741a7af2ff1735f812fe9a9
Elijah <edc8230@g.rit.edu>
2019-12-03 19:39:17 -0500

    Refactored the List subsystem. Added Entry class.

--- model/artifact.py
+++ model/artifact.py
@@ -1,7 +1,8 @@
 import enum
+from model.list_pkg.entry import Entry
 
 
-class Artifact():
+class Artifact(Entry):
 
     class ArtifactType(enum.Enum):
         PAPER = "Paper"
@@ -13,7 +14,7 @@ class Artifact():
         self._corresponding_id = corresponding_id
         self._artifact_type = artifact_type
         self._author_id = author_id
-        self.artifact_name = artifact_name
+        self._artifact_name = artifact_name
 
         # Attributes for Papers Only
         self._title = None
@@ -21,13 +22,13 @@ class Artifact():
         self._version = None
         self._topic = None
 
-    def create_artifact_hash(self):
+    def create_entry_dictionary(self):
         if self._artifact_type == Artifact.ArtifactType.PAPER:
             return {"artifactID": self._artifact_id,
                     "correspondingID": self._corresponding_id,
                     "type": self._artifact_type.value,
                     "authorID": self._author_id,
-                    "artifactName": self.artifact_name,
+                    "artifactName": self._artifact_name,
                     "title": self._title,
                     "authors": self._authors,
                     "version": self._version,
@@ -38,14 +39,26 @@ class Artifact():
                     "correspondingID": self._corresponding_id,
                     "type": self._artifact_type.value,
                     "authorID": self._author_id,
-                    "artifactName": self.artifact_name
+                    "artifactName": self._artifact_name
                     }
 
-    def get_id(self):
+    def get_entry_id(self):
         return self._artifact_id
 
-    def set_paper_attributes(self, title: str, authors: str, version: float, topic: str):
-        self._title = title
-        self._authors = authors
-        self._version = version
-        self._topic = topic
+    def set_entry_attributes(self, attributes: {}):
+        self._artifact_id = attributes.get("artifactID")
+        self._corresponding_id = attributes["correspondingID"]
+        self._author_id = attributes["authorID"]
+        self._artifact_name = attributes["artifactName"]
+        type = attributes["type"]
+
+        if type == Artifact.ArtifactType.PAPER.value:
+            self._artifact_type = Artifact.ArtifactType.PAPER
+            self._title = attributes["title"]
+            self._authors = attributes["authors"]
+            self._version = attributes["version"]
+            self._topic = attributes["topic"]
+        elif type == Artifact.ArtifactType.REVIEW.value:
+            self._artifact_type = Artifact.ArtifactType.REVIEW
+        elif type == Artifact.ArtifactType.REPORT.value:
+            self._artifact_type = Artifact.ArtifactType.REPORT
