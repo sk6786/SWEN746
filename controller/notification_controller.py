364af81675257548ffc6b32439882765741a1217
Elijah <edc8230@g.rit.edu>
2019-12-04 21:31:15 -0500

    Updated controler to update objects in monogo.

--- controller/notification_controller.py
+++ controller/notification_controller.py
@@ -53,15 +53,28 @@ class NotificationController:
         }
 
     def update_all_deadlines(self, deadlines: {}):
-        self._accounts.get_entry(NotificationController.NotificationID.AUTHOR_ID.value).set_deadline(deadlines["Author"])
-        self._accounts.get_entry(NotificationController.NotificationID.PCM_ID.value).set_deadline(deadlines["PCM"])
-        self._accounts.get_entry(NotificationController.NotificationID.PCC_ID.value).set_deadline(deadlines["PCC"])
-        self._accounts.get_entry(NotificationController.NotificationID.ADMIN_ID.value).set_deadline(deadlines["Admin"])
+        new_author_dead = self._accounts.get_entry(NotificationController.NotificationID.AUTHOR_ID.value)
+        new_author_dead.set_dealine(deadlines["Author"])
+        self._accounts.update_entry(NotificationController.NotificationID.AUTHOR_ID.value, new_author_dead)
+
+        new_pcm_dead = self._accounts.get_entry(NotificationController.NotificationID.PCM_ID.value)
+        new_pcm_dead.set_deadline(deadlines["PCM"])
+        self._accounts.update_entry(NotificationController.NotificationID.PCM_ID.value, new_pcm_dead)
+
+        new_pcc_dead = self._accounts.get_entry(NotificationController.NotificationID.PCC_ID.value)
+        new_pcc_dead.set_deadline(deadlines["PCC"])
+        self._accounts.update_entry(NotificationController.NotificationID.PCC_ID.value, new_pcc_dead)
+
+        new_admin_dead = self._accounts.get_entry(NotificationController.NotificationID.ADMIN_ID.value)
+        new_admin_dead.set_deadline(deadlines["Admin"])
+        self._accounts.update_entry(NotificationController.NotificationID.ADMIN_ID.value, new_admin_dead)
 
     def delete_notification(self, account_id: int, notification: str):
         account = self._accounts.get_entry(account_id)
         account.remove_notification(notification)
+        self._accounts.update_entry(account_id, account)
 
     def add_notification(self, account_id: int, notification: str):
         account = self._accounts.get_entry(account_id)
         account.add_notification(notification)
+        self._accounts.update_entry(account_id, account)
